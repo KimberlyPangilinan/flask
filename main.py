d4b233f4e53a4ebff80d96f04848a1a0aa1ff32f
KimberlyPangilinan <kimberlypangilinan2001@gmail.com>
2023-12-20 09:02:40 +0800

    fix: first name lastname format

--- main.py
+++ main.py
@@ -426,7 +426,7 @@ def recommend_and_add_to_history():
                 CASE WHEN LOGS.type = 'download' THEN 1 ELSE 0
             END
             ) AS total_downloads,
-            c.contributors
+            c.contributors, c.contributors_A
             FROM
                 article
             LEFT JOIN journal ON article.journal_id = journal.journal_id
@@ -435,7 +435,9 @@ def recommend_and_add_to_history():
             LEFT JOIN(
                 SELECT article_id,
                     GROUP_CONCAT(
-                        DISTINCT CONCAT(lastname,' ',SUBSTRING(firstname, 1, 1), '.','->',orcid) SEPARATOR ', ') AS contributors
+                        DISTINCT CONCAT(lastname,', ',SUBSTRING(firstname, 1, 1), '.','->',orcid) SEPARATOR ' and ') AS contributors,
+                    GROUP_CONCAT(
+                        DISTINCT CONCAT(lastname,', ',firstname,'->',orcid) SEPARATOR ' and ') AS contributors_A
                 FROM
                     contributor
                 GROUP BY
