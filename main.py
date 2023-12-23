88656284d85b86fdffe454d42f5b2fb1b6f805b2
KimberlyPangilinan <kimberlypangilinan2001@gmail.com>
2023-12-23 13:57:30 +0800

    feat: try

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask,session
 from flask_cors import CORS 
 from routes.articles import articles_bp
 from routes.recommendations import recommendations_bp
 from routes.check import check_bp
-
+from datetime import timedelta
 app = Flask(__name__)
-CORS(app) 
+
 app.json.sort_keys = False
+app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = True
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=5)
+app.config["SESSION_TYPE"] = "filesystem"
+
+# app.config['MAIL_USE_TLS'] =  os.getenv("MAIL_USE_TLS")
+app.config["SESSION_COOKIE_SAMESITE"] = "None"
+app.config["SESSION_COOKIE_SECURE"] = True
+CORS(app)
 
 
 # Register blueprints
