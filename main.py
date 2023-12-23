798215b27f1d7cc02b29745a5076b9ce3d93fbfc
KimberlyPangilinan <kimberlypangilinan2001@gmail.com>
2023-12-23 14:09:19 +0800

    fix: add api

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,session
+from flask import Flask
 from flask_cors import CORS 
 from routes.articles import articles_bp
 from routes.recommendations import recommendations_bp
@@ -8,20 +8,13 @@ app = Flask(__name__)
 
 app.json.sort_keys = False
 app = Flask(__name__)
-app.config["SESSION_PERMANENT"] = True
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=5)
-app.config["SESSION_TYPE"] = "filesystem"
-
-# app.config['MAIL_USE_TLS'] =  os.getenv("MAIL_USE_TLS")
-app.config["SESSION_COOKIE_SAMESITE"] = "None"
-app.config["SESSION_COOKIE_SECURE"] = True
 CORS(app)
 
 
 # Register blueprints
-app.register_blueprint(articles_bp, url_prefix='/articles')
-app.register_blueprint(recommendations_bp, url_prefix='/recommendations')
-app.register_blueprint(check_bp, url_prefix='/check')
+app.register_blueprint(articles_bp, url_prefix='/api/articles')
+app.register_blueprint(recommendations_bp, url_prefix='/api/recommendations')
+app.register_blueprint(check_bp, url_prefix='/api/check')
 
 
 if __name__ == '__main__':
