b8e047db4cc8ef4d46149e3f347c6913bb175b1b
KimberlyPangilinan <kimberlypangilinan2001@gmail.com>
2024-01-23 13:01:36 +0800

    feat: fetch all journal

--- routes/journal.py
+++ routes/journal.py
@@ -2,19 +2,18 @@ from flask import Flask, request, jsonify,Blueprint
 from db import db
 
 journal_bp = Blueprint('journal',__name__)
+
 @journal_bp.route('/', methods=['GET'])
 def get_journal():
-    param = request.args.get('id',"")
+    param = request.args.get('id')
 
-   
-
-    db.ping(reconnect=True)
-    cursor = db.cursor()
-    journal_sql = 'SELECT * FROM journal WHERE journal_id LIKE %s'
-
-    cursor.execute(journal_sql, ('%' + param + '%',))
-    journal_details = cursor.fetchall()
-
-    return jsonify(journal_details)
-    
-
+    try:
+        with db.cursor() as cursor:
+            if param is None:
+                cursor.execute('SELECT * FROM journal')
+                return jsonify({"journal": cursor.fetchall()})
+            else:
+                cursor.execute('SELECT * FROM journal WHERE journal_id = %s', (param,))
+                return jsonify({"journalDetails": cursor.fetchone()})
+    except Exception as e:
+        return jsonify({"error": str(e)})
